06f12d9b4aabb1947c2b7b7c3ee728ce4836839f
Rohith <rohithsakthit@gmail.com>
2024-06-25 17:15:33 +0530

    Reverse String rohith added

---
+++ ReverseString_8118264.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+ 
+public class Test {
+    public static void main(String[] args)
+    {
+        String str = "Geeks";
+ 
+        // conversion from String object to StringBuffer
+        StringBuffer sbr = new StringBuffer(str);
+        // To reverse the string
+        sbr.reverse();
+        System.out.println(sbr);
+    }
+}
